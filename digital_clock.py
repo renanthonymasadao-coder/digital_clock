e0dad741bb324f51c02c3a69fd5bccd75de2c32d
renanthonymasadao-coder <renanthony.masadao@gmail.com>
2026-03-02 23:01:17 +0800

    updated it to real- time or world clock

--- digital_clock.py
+++ digital_clock.py
@@ -9,11 +9,13 @@ def update_clock():
     seconds = time.strftime("%S")
     am_or_pm = time.strftime("%p")
     time_text = hours + ":" + minutes + ":" + seconds + " " + am_or_pm
-    digital_clock.config(text=time_text)
+    digital_clock_lbl.config(text=time_text)
+
 
 digital_clock_lbl = ui.Label(window, text="00:00:00:",
                              font="Helvetica 72 bold")
-
 digital_clock_lbl.pack()
+
+update_clock()
 
 window.mainloop()
